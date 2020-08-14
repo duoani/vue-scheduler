8beb8ad383c010d1b4824426198cf2a1de4196c1
duoani <duoani@126.com>
2020-08-14 10:49:43 +0800

    export Scheduler component

--- src/index.js
+++ src/index.js
@@ -1,24 +1,22 @@
 import Scheduler from '@/components/Scheduler'
 import { setLocale } from './utils/i18n'
 
-const Plugin = {
-  install(Vue, options = {}) {
-    options = Object.assign({
-      componentName: 'Scheduler' // you can customize the component tag name
-    }, options)
-    const { locale, encoder, decoder } = options
-    if (typeof locale === 'object') {
-      setLocale(locale)
-    }
-
-    Vue.component(options.componentName, Scheduler)
-
-    Vue.prototype.$SCHEDULER = { encoder, decoder }
+Scheduler.install = function(Vue, options = {}) {
+  options = Object.assign({
+    componentName: 'Scheduler' // you can customize the component tag name
+  }, options)
+  const { locale, encoder, decoder } = options
+  if (typeof locale === 'object') {
+    setLocale(locale)
   }
+
+  Vue.component(options.componentName, Scheduler)
+
+  Vue.prototype.$SCHEDULER = { encoder, decoder }
 }
 
 if (typeof window !== 'undefined' && window.Vue) {
-  Plugin.install(window.Vue)
+  Scheduler.install(window.Vue)
 }
 
-export default Plugin
+export default Scheduler
